fb8668a5abf6e17b5c757b63e4eacca141777da4
wainlo <s10265915@connect.np.edu.sg>
2025-06-11 23:51:40 +0800

    trying to set respawn for fire

---
+++ Assets/respawn.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class respan : MonoBehaviour
+{
+    public Transform startPoint; // Assign this in the Inspector
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("fire"))
+        {
+            transform.position = startPoint.position;
+        }
+    }
+}
+
