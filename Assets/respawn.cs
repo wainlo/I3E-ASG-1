04d9a14b7c74732ca721578df09747872e797d19
wainlo <s10265915@connect.np.edu.sg>
2025-06-12 03:48:46 +0800

    key asset

--- Assets/respawn.cs
+++ Assets/respawn.cs
@@ -15,7 +15,7 @@ public class respan : MonoBehaviour
     {
 
     }
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("fire"))
         {
